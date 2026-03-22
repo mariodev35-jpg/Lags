a53e27ebf6cacdb196e2c75a310e21450abb0add
mariodev35-jpg <mariodev35@gmail.com>
2026-03-21 23:03:56 -0400

    Last Sync: 2026-03-21 23:03 (Mobile)

---
+++ juego para la jam/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+@export var scene : String = "res://map1.tscn"
+
+
+func _on_btnplay_pressed() -> void:
+	Global.change_scene = scene
+	get_tree().change_scene_to_file("res://barradecarga.tscn")
+
+func _on_btnconfi_pressed() -> void:
+	pass
+
+
+func _on_btnexit_pressed() -> void:
+	get_tree().quit()
