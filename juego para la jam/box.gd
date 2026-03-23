0a7ea0b5327292546c183a1c1f1fd4be3e5f5265
mariodev35-jpg <mariodev35@gmail.com>
2026-03-23 02:06:00 -0400

    Last Sync: 2026-03-23 02:05 (Mobile)

--- juego para la jam/box.gd
+++ juego para la jam/box.gd
@@ -1,11 +1,21 @@
 extends RigidBody3D
+var topy_box = 1
 var gara = false
 var posi 
+func _ready() -> void:
+	topy_box = randi_range(1,2)
+	if topy_box == 1 :
+		$box1.show()
+		$box2.hide()
+	elif topy_box == 2:
+		$box1.hide()
+		$box2.show()
 func agar(pos):
 	gara = true
 	posi = pos
 func soltar():
 	gara = false
+
 func _process(delta: float) -> void:
 	if gara == true:
 		sleeping =true
