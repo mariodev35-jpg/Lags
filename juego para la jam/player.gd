717a152396a78d1ff70532326abb5cec42320d00
jcatota <josue_ca456@hotmail.com>
2026-03-25 17:09:47 -0500

    Agregado Mapa2

--- juego para la jam/player.gd
+++ juego para la jam/player.gd
@@ -79,7 +79,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var tween = create_tween()
+	#var tween = create_tween()
 	if Global.stoplight == true and !direction and alumbra == false:
 		
 		$AnimationPlayer.play("osvu")
